3296c8172909206ec528a191dda6d970a9fbe4d3
ducvu <vuduc2701@gmail.com>
2021-12-03 16:24:44 +0700

    1234

--- netCore/Migrations/20211203080412_1.Designer.cs
+++ netCore/Migrations/20211203080412_1.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using netCore.Data;
 
 namespace netCore.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20211203080412_1")]
+    partial class _1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- netCore/Models/Movie.cs
+++ netCore/Models/Movie.cs
@@ -7,12 +7,27 @@ namespace netCore.Models
     public class Movie
     {
         [Key]
-          public int Id { get; set; }
+        public int Id { get; set; }
+        [Required(ErrorMessage = "không được để trống")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "tối thiểu có 3 ký tự")]
+        // ErrorMessage = "không được để trống"
         public string Title { get; set; }
-
+        [Required(ErrorMessage = "không được để trống")]
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "không được để trống")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(30)]
         public string Genre { get; set; }
+        [Range(1, 100)]
+        [Required(ErrorMessage = "không được để trống")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "không được để trống")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "không được để trống")]
+        [StringLength(5)]
+        public string Rating { get; set; }
     }
 }
